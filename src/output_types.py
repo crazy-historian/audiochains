37eaec3425adb9ee2aaf0de93fabbcee0dbe23ae
crazy_historian <zaitsev808@mail.ru>
2021-10-20 17:32:21 +0300

    fixed numpy typing due to numpy 1.20 version

--- src/output_types.py
+++ src/output_types.py
@@ -2,14 +2,12 @@
     The definition of BlockAudioMethod possible output data types.
 """
 
-
 import numpy as np
-import numpy.typing as npt
 from dataclasses import dataclass
 from enum import IntEnum
 
-np_int16_array = npt.NDArray[np.int16]
-np_float32_array = npt.NDArray[np.float32]
+np_int16_array = np.ndarray
+np_float32_array = np.ndarray
 
 
 @dataclass
