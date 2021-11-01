cb365e00f6f0625b58872c1551c5a96922d2ca5c
crazy_historian <zaitsev808@mail.ru>
2021-11-01 12:39:30 +0300

    commented implemented BlockAudioMethods

--- src/block_methods.py
+++ src/block_methods.py
@@ -15,17 +15,33 @@ from output_types import (
 
 
 class BlockAudioMethod(ABC):
+    """
+    An abstract interface defining the functionality of audio processing unit.
+    """
     @abstractmethod
     def __call__(self, in_data):
+        """
+        The developer have to override __call__ method
+        which performs audio processing on data block (chunk)
+        ----
+        Parameters:
+            in_data: input audio data block
+        """
         ...
 
 
 class UnpackRawInInt16(BlockAudioMethod):
+    """
+    Unpacking raw audio data (sequences of bytes) in numpy int16 array.
+    """
     def __call__(self, in_data: bytes) -> np_int16_array:
         return np.frombuffer(in_data, np.int16)
 
 
 class UnpackRawInFloat32(BlockAudioMethod):
+    """
+    Unpacking raw audio data (sequences of bytes) in the numpy float32 array.
+    """
     def __call__(self, in_data: bytes) -> np_float32_array:
         max_int16_value = 2 ** 15
         data = np.frombuffer(in_data, np.int16)
@@ -33,6 +49,9 @@ class UnpackRawInFloat32(BlockAudioMethod):
 
 
 class RMSFromBytes(BlockAudioMethod):
+    """
+    Calculating root mean square (RMS) value of the input raw audio block with a certain sample width.
+    """
     def __init__(self, width: int = 2):
         self.width = width
 
@@ -41,12 +60,18 @@ class RMSFromBytes(BlockAudioMethod):
 
 
 class RMSFromArray(BlockAudioMethod):
+    """
+    Calculating root mean square (RMS) value of the input numpy array.
+    """
     def __call__(self, in_data: Union[np_int16_array, np_float32_array]) -> int:
         in_data = in_data.astype(np.float32)
         return round(np.sqrt((in_data * in_data).sum() / len(in_data)))
 
 
-class RMSlog(BlockAudioMethod):
+class DBLog10(BlockAudioMethod):
+    """
+    Calculating logarithmic relative value of input int vale
+    """
     def __call__(self, in_data: int):
         if in_data > 0:
             return round(20 * log10(in_data), 2)
@@ -55,11 +80,17 @@ class RMSlog(BlockAudioMethod):
 
 
 class HammingWindow(BlockAudioMethod):
+    """
+    Superimposing the hamming window on the input numpy array.
+    """
     def __call__(self, data: Union[np_int16_array, np_float32_array]) -> np_float32_array:
         return data * np.hamming(len(data))
 
 
 class FourierTransform(BlockAudioMethod):
+    """
+    Calculating the fourier transform on the input numpy float32 array with a certain framerate.
+    """
     def __init__(self, framerate: int):
         self.framerate = framerate
 
@@ -72,6 +103,9 @@ class FourierTransform(BlockAudioMethod):
 
 
 class MFCC(BlockAudioMethod):
+    """
+    Calculating certain number of mfcc coefficients of the input numpy float32 array
+    """
     def __init__(self, n_mfcc: int, freq_rate: int):
         super().__init__()
         self.n_mfcc = n_mfcc
@@ -82,6 +116,9 @@ class MFCC(BlockAudioMethod):
 
 
 class BandPassFilter(BlockAudioMethod):
+    """
+    Application of the butterworth bandpass filter
+    """
     def __init__(self,
                  sample_rate: int,
                  low_cut: int = 200,
@@ -99,6 +136,9 @@ class BandPassFilter(BlockAudioMethod):
 
 
 class SoundPressureThreshold(BlockAudioMethod):
+    """
+    Calculation of the threshold function to the RMS value of the input audio amplitude
+    """
     def __init__(self,
                  silence_diapason: List[int],
                  whisper_diapason: List[int],
