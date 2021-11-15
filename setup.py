9e433db8ba0ced8a179756ee412b609e7e3ccc15
Максим <zaitsev808@mail.ru>
2021-11-16 00:40:58 +0300

    renamed the project.

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
+from pathlib import Path
 from setuptools import setup
 
-README = ''
+project_path = Path(__file__).parent
+
+README = str(Path(f'{project_path}/test/test_config.json'))
+with open(README, encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name='runtime_audio_processing',
+    name='audiochains',
     description='runtime audio processing in chains of algorithms',
     version='0.9.0',
     license='',
-    long_description=README,  # TODO: write markdown description!
+    long_description=long_description,
     long_desctiption_content_type='text/markdown',
     url='https://github.com/crazy-historian/runtime_audio_processing',
     author='Maxim Zaitsev',
     author_email='zaitsev808@mail.ru',
 
-    packages=[''],
+    packages=['test'],
+    package_data={'/test': ['test_recording.wav', 'test_playback.wav', 'test_config.json']},
+    include_package_data=True,
     install_requires=[
         "numpy==1.20.0",
         "scipy>=1.7.1",
@@ -24,5 +31,5 @@ setup(
         "pytest~=6.2.5"
     ],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest']  # TODO: figure out what is a setup requires, test requires
+    tests_require=['pytest']
 )
