a249c1060e0ed7ee0ce984d19695869382827673
Максим <zaitsev808@mail.ru>
2021-11-17 17:59:54 +0300

    patch 0.1.2: audiochains.threads package was added: it includes decorator and class wrapping for thread execution.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(README, encoding='utf-8') as f:
 setup(
     name='audiochains',
     description='runtime audio processing in chains of algorithms',
-    version='0.1.1',
+    version='0.1.2',
     license='',
     long_description=long_description,
     long_desctiption_content_type='text/markdown',
